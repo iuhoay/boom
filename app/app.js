38db536d1674ec3677ea6647527146cc1f915516
iuhoay <iuhoay@gmail.com>
2016-06-08 10:05:39 +0800

    cloud data

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-new Vue({
+var app = new Vue({
   el: '#app',
   data: {
     title: '暴暴 (boom)',
@@ -6,29 +6,33 @@ new Vue({
       name: '暴暴',
       avatar: 'asserts/images/dog.png',
       birthday: '2016-02-07',
-      weights: [
-        {
-          date: '2016-06-04',
-          weight: '5',
-        }
-      ]
     },
-    dry_foods: [
-      {
-        name: '宝路',
-        review: '不推荐',
-        describe: '打死也不让暴暴吃宝路了，拉屎太 TM 臭了，远远就闻到，立马换了。',
-      },
-      {
-        name: '海洋之星 (Fish4dogs) 幼犬粮 深海鱼',
-        review: '★★★★',
-        describe: '浓浓的鱼腥味，暴暴还能接受，拉屎不那么臭了。',
-      },
-      {
-        name: 'GO! 幼犬粮 六种肉五谷 天然粮',
-        review: '',
-        describe: '等着到货，买了大包，小包不经吃'
-      }
-    ],
+    weights: [],
+    dry_foods: [],
+  }
+})
+
+AV.init({appId: '7WYEcW832su5uxMBUqxNMHkd-gzGzoHsz', appKey: 'U53zqQdHn2F72XjOQHVSFIP8'});
+
+new AV.Query('dry_food').find().then(function(results) {
+  var dry_food;
+  for (index in results) {
+    dry_food = results[index];
+    app.dry_foods.push({
+      name: dry_food.get('name'),
+      review: dry_food.get('review'),
+      describe: dry_food.get('describe'),
+    })
+  }
+})
+
+new AV.Query('weights').find().then(function(results) {
+  var weight;
+  for (i in results) {
+    weight = results[i];
+    app.weights.push({
+      date: weight.get('date'),
+      weight: weight.get('weight'),
+    })
   }
 })
